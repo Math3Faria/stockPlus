cfe76ba3f19aeae7481ca6cf0a2fe8fc39b2591f
Math3Faria <matheusteud12@gmail.com>
2026-04-22 10:37:44 -0300

    arrumando as rotas

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,30 +1,17 @@
 import { Router } from "express";
-<<<<<<< HEAD
+
 import produtoRoutes from "./produto.routes";
-<<<<<<< HEAD
 import estoqueRoutes from "./estoque.routes";
 import lotesRoutes from "./lotes.routes";
-=======
 import movimentacaoRoutes from "./movimentacaoEstoque.routes";
->>>>>>> 87655de376508c5804483e208c78156a21d07b50
-
-const router = Router();
-
-router.use(produtoRoutes);
-<<<<<<< HEAD
-router.use(estoqueRoutes);
-router.use(lotesRoutes);
-=======
-import categoriaRoutes from "./categoria.routes";
 import fornecedorRoutes from "./fornecedores.routes";
 
 const router = Router();
 
-router.use('/', categoriaRoutes);
-router.use('/', fornecedorRoutes);
->>>>>>> f38bf5c3843a92aa8bd4a5d606e0c4d963aff682
-=======
+router.use(produtoRoutes);
+router.use(estoqueRoutes);
+router.use(lotesRoutes);
 router.use(movimentacaoRoutes);
->>>>>>> 87655de376508c5804483e208c78156a21d07b50
+router.use(fornecedorRoutes);
 
 export default router;
